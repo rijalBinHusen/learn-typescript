5d0c05eac2b5f2ae0bde789f6a1a143f8552ea71
rijal <jeryzal333@gmail.com>
2022-04-17 20:47:14 +0700

    Arrays and Objects

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,30 @@
-var character = "Rijal";
-var age = 30;
-var isBlackBelt = false;
-// character = 20 //error, cant replace string with number
-character = "Bin husen";
-// age = "rijal" //error, cant replace number with string
-age = 40;
-// isBlackBelt = "yes" //error, cant replace boolean with string or number
-isBlackBelt = true;
-// We just can replace the original variable with the same type
-// define the type of parameter
-var circ = function (diameter) {
-    return diameter * Math.PI;
+// arrays
+var names = ["Rijal", "Muhammad", "Abdulloh", "Umar", "Ali", "Usman"];
+//names = "" // error cant replace array with string
+names.push("Hamzah");
+// names.push(3) //error, you cant push number to array of string
+// names[0] = 3 //error, you cant push number to array of string
+var numbers = [1, 2, 3, 4, 5, 6];
+// numbers.push("Str")  //error, you cant push Strring to array of number
+// numbers[0] = "Str" //error, you cant push string to array of number
+// Mix array number and string
+var mixed = [1, 2, 3, "Rijal", "Str", "4", "sdk"];
+mixed.push("Husen");
+mixed.push(10);
+mixed[0] = "String";
+// Objects
+var group = {
+    name: "rijal",
+    age: 27,
+    isGood: true
 };
-console.log(circ(7));
+group.age = 3;
+group.name = "Husen";
+// group.age = "123" //error, cant replace number with string
+// group.skill = ["Javascript", "HTML", "CSS"]// error, because skill doesnt exist
+// group = {
+//   name: "rijal",
+//   age: 27,
+//   isGood: true,
+//   skills: ["JS"] // error because skills doesnt exist on the original object
+// }
--- sandbox.ts
+++ sandbox.ts
@@ -1,21 +1,38 @@
-let character = "Rijal"
-let age = 30;
-let isBlackBelt = false;
+// arrays
+let names = ["Rijal", "Muhammad", "Abdulloh", "Umar", "Ali", "Usman"]
 
-// character = 20 //error, cant replace string with number
-character = "Bin husen";
+//names = "" // error cant replace array with string
 
-// age = "rijal" //error, cant replace number with string
-age = 40;
+names.push("Hamzah")
+// names.push(3) //error, you cant push number to array of string
+// names[0] = 3 //error, you cant push number to array of string
 
-// isBlackBelt = "yes" //error, cant replace boolean with string or number
-isBlackBelt = true
+let numbers = [1,2,3,4,5,6]
+// numbers.push("Str")  //error, you cant push Strring to array of number
+// numbers[0] = "Str" //error, you cant push string to array of number
 
-// We just can replace the original variable with the same type
+// Mix array number and string
+let mixed = [1,2,3, "Rijal", "Str", "4", "sdk"]
 
-// define the type of parameter
-const circ = (diameter: number) => {
-  return diameter * Math.PI
+mixed.push("Husen")
+mixed.push(10)
+mixed[0] = "String"
+
+// Objects
+let group = {
+  name: "rijal",
+  age: 27,
+  isGood: true,
 }
 
-console.log(circ(7))
+group.age = 3
+group.name = "Husen"
+// group.age = "123" //error, cant replace number with string
+// group.skill = ["Javascript", "HTML", "CSS"]// error, because skill doesnt exist
+
+// group = {
+//   name: "rijal",
+//   age: 27,
+//   isGood: true,
+//   skills: ["JS"] // error because skills doesnt exist on the original object
+// }

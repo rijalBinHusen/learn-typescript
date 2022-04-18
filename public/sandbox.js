3fb3fb63330d6f2f27dfb379e9ffc3f25978e103
rijal <jeryzal333@gmail.com>
2022-04-18 20:31:22 +0700

    function basic

--- public/sandbox.js
+++ public/sandbox.js
@@ -9,3 +9,8 @@ const add = (a, b, c = 10) => {
     console.log(c);
 };
 add(3, 77, 22);
+const minus = (a, b) => {
+    return a + b;
+};
+let result = minus(10, 7);
+// result = "string" // error, because result type is number
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -11,3 +11,10 @@ const add = (a: number, b: number, c: number | string = 10) => {
 }
 
 add(3, 77, 22)
+
+const minus = (a: number, b:number) => {
+    return a + b
+}
+
+let result = minus(10, 7)
+// result = "string" // error, because result type is number

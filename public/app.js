3a8817eac180ffae56140c2c23b2ae344d2ae1d4
rijal <jeryzal333@gmail.com>
2022-05-21 05:17:12 +0700

    interfaces with classes 2

--- public/app.js
+++ public/app.js
@@ -1,47 +1,6 @@
-// interfaces
-// interface IsPerson {
-//     name: string,
-//     age:  number,
-//     speak(a: string): void;
-//     spend(a: number): number;
-// }
-// const me: IsPerson = {
-//     name: "rijal",
-//     age: 27,
-//     speak(text: string): void{
-//         console.log(text)
-//     },
-//     spend(amount: number): number{
-//         console.log("I spend", amount)
-//         return amount
-//     }
-// }
-// const somePerson = (person: IsPerson) => {
-//     console.log("Hello, ", person.name)
-// }
-// somePerson(me);
-// console.log(me)
 import { Payment } from "./classes/Payment.js";
 import { Invoice } from "./classes/Invoice.js";
-let docOne;
-let docTwo;
-docOne = new Invoice("Rijal", "web work", 250);
-docTwo = new Payment("Husen", "plumbing work", 200);
-let docs = [];
-docs.push(docOne);
-docs.push(docTwo);
-console.log(docs);
-const invOne = new Invoice("Rijal", "Work on the Rijal website", 250);
-const invTwo = new Invoice("Husen", "Work on the Husen website", 300);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-invoices.forEach((inv) => {
-    console.log(inv.client, inv.amount, inv.format());
-});
-// const form = document.querySelewoctor("form")!;
 const form = document.querySelector(".new-item-form");
-// console.log(form.children)
 // inputs
 const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
@@ -49,5 +8,12 @@ const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.value);
+    let doc;
+    if (type.value === "invoice") {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,61 +1,8 @@
-// interfaces
-// interface IsPerson {
-//     name: string,
-//     age:  number,
-//     speak(a: string): void;
-//     spend(a: number): number;
-// }
-
-// const me: IsPerson = {
-//     name: "rijal",
-//     age: 27,
-//     speak(text: string): void{
-//         console.log(text)
-//     },
-//     spend(amount: number): number{
-//         console.log("I spend", amount)
-//         return amount
-//     }
-// }
-
-// const somePerson = (person: IsPerson) => {
-//     console.log("Hello, ", person.name)
-// }
-
-// somePerson(me);
-
-// console.log(me)
 import { Payment } from "./classes/Payment.js"
 import { Invoice } from "./classes/Invoice.js"
 import { HasFormatter } from "./interfaces/HasFormatter.js";
 
-let docOne: HasFormatter;
-let docTwo: HasFormatter;
-
-docOne = new Invoice("Rijal", "web work", 250);
-docTwo = new Payment("Husen", "plumbing work", 200)
-
-let docs: HasFormatter[] = [];
-docs.push(docOne);
-docs.push(docTwo)
-
-console.log(docs)
-
-const invOne = new Invoice("Rijal", "Work on the Rijal website", 250);
-const invTwo = new Invoice("Husen", "Work on the Husen website", 300);
-
-let invoices: Invoice[] = [];
-invoices.push(invOne)
-invoices.push(invTwo)
-
-invoices.forEach((inv) => {
-    console.log(inv.client, inv.amount, inv.format())
-})
-
-// const form = document.querySelewoctor("form")!;
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
-
-// console.log(form.children)
 
 // inputs
 const type = document.querySelector("#type") as HTMLSelectElement;
@@ -65,10 +12,14 @@ const amount = document.querySelector("#amount") as HTMLInputElement;
 
 form.addEventListener("submit", (e: Event) => {
     e.preventDefault();
-    console.log(
-        type.value,
-        tofrom.value,
-        details.value,
-        amount.value,
-    )
+
+    let doc: HasFormatter;
+    if(type.value === "invoice") {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+    }
+
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
+    }
+    console.log(doc)
 })
